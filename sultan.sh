108c22fbcabc03401470e577666da6fa7d5fa2c6
R4D3N <41930017+ganucisystem@users.noreply.github.com>
2018-12-05 17:38:21 +0700

    Update sultan.sh

--- sultan.sh
+++ sultan.sh
@@ -39,27 +39,6 @@ echo -b "
  :   : :       :::   :: :  :    : : :   ::    :   
                                                   ";
 echo "";
-#jangan recode 
-#jangan copas
-#pake aja langsung
-#dengan recode gak bikin lu jadi jago
-#camkan itu ferguso
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
-
 figlet RECEH | lolcat
 
 echo -b " CODED BY R4D3N G0Z4LL $green " |lolcat
